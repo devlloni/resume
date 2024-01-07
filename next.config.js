57f728fac105e755ac8ada41f1fdb54da91822a2
Federico Mahfoud <fedemahf@users.noreply.github.com>
2024-01-07 01:14:09 -0300

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // Create `out` folder with static website using `yarn build`
+  output: 'export',
+}
 
 module.exports = nextConfig
