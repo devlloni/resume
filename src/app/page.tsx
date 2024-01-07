8227fdb2ff5ba9958336d522c85fd46e827535e4
Federico Mahfoud <fedemahf@users.noreply.github.com>
2024-01-07 09:33:21 -0300

    Work link optional and target blank

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -118,9 +118,11 @@ export default function Page() {
                 <CardHeader>
                   <div className="flex items-center justify-between gap-x-2 text-base">
                     <h3 className="inline-flex items-center justify-center gap-x-1 font-semibold leading-none">
-                      <a className="hover:underline" href={work.link}>
+                      {work.link ? (
+                      <a className="hover:underline" href={work.link} target="_blank">
                         {work.company}
                       </a>
+                      ) : work.company}
 
                       <span className="inline-flex gap-x-1">
                         {work.badges.map((badge) => (
